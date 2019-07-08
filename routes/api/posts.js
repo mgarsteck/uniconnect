a282b41660f01bb6a943ad8257bf8d2b1188aa1d
mgarsteck <garsteckphotography@gmail.com>
2019-07-08 11:14:07 -0700

    POST like/unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -103,5 +103,49 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+// PUT api/posts/like/:id, Like a post, Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        
+        // Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: "POST ALREADY LIKED" })
+        }
+
+        post.likes.unshift({ user: req.user.id })
+        await post.save()
+        res.json(post.likes)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('SERVER ERROR')
+    }
+})
+
+// PUT api/posts/unlike/:id, unLike a post, Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        
+        // Check if the post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: "YOU NEED TO LIKE THE POST FIRST..." })
+        }
+
+        // GET REMOVE INDEX
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
+
+        post.likes.splice(removeIndex, 1)
+        
+        await post.save()
+        res.json(post.likes)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('SERVER ERROR')
+    }
+})
+
 
 module.exports = router
